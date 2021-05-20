9f62a844b4865f127fdee291857a9a706e7d4169
Matt Tucker <matt.watkinsmedia.org>
2021-05-20 11:49:10 +0100

    Correct camelcase for LOA variant

--- src/main/scala/variant/Variant.scala
+++ src/main/scala/variant/Variant.scala
@@ -29,7 +29,7 @@ abstract class Variant private[variant] (
   def horde         = this == Horde
   def racingKings   = this == RacingKings
   def crazyhouse    = this == Crazyhouse
-  def linesofaction = this == LinesOfAction
+  def linesOfAction = this == LinesOfAction
 
   def exotic = !standard
 
