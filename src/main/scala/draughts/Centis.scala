e61305e886ed27a02aad68d443495f0561249d4a
Lakin Wecker <lakin.wecker@gmail.com>
2021-07-03 11:33:11 -0600

    Whoops, one file fix. :D

--- src/main/scala/draughts/Centis.scala
+++ src/main/scala/draughts/Centis.scala
@@ -36,9 +36,10 @@ final case class Centis(centis: Int) extends AnyVal with Ordered[Centis] {
 
 object Centis {
   implicit final val zeroInstance = Zero.instance(Centis(0))
-  implicit object CentisMonoid extends Monoid[Centis] {
-    def append(c1: Centis, c2: => Centis) = c1 + c2
-    final val zero                        = Centis(0)
+
+  implicit val CentisMonoid = new Monoid[Centis] {
+    def combine(c1: Centis, c2: Centis) = c1 + c2
+    final val empty                     = Centis(0)
   }
 
   implicit final class CentisScalar(val scalar: Int) extends AnyVal {
