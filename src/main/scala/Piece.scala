54786c365116833f2d5c5bdb1f338029af0ed5a2
Matt Tucker <matt.watkinsmedia.org>
2021-07-10 14:13:22 +0100

    Add Piece to the wrapper

---
+++ src/main/scala/Piece.scala
@@ -0,0 +1,38 @@
+package strategygames
+
+sealed class Piece(color: Color, role: Role) {
+
+  def is(c: Color)    = c == color
+  def is(r: Role)     = r == role
+  def isNot(c: Color) = c != color
+  def isNot(r: Role)  = r != role
+
+  def oneOf(rs: Set[Role]) = rs(role)
+
+  def forsyth: Char
+
+  override def toString = s"$color-$role".toLowerCase
+
+}
+
+object Piece {
+
+  final case class Chess(p: chess.Piece) extends Piece(
+    Color.Chess(p.color),
+    Role.Chess(p.role)
+  ) {
+
+    def forsyth: Char = p.forsyth
+
+  }
+
+  final case class Piece(p: draughts.Piece) extends Piece(
+    Color.Draughts(p.color),
+    Role.Draughts(p.role)
+  ){
+
+    def forsyth: Char = p.forsyth
+
+  }
+
+}
--- src/main/scala/chess/Piece.scala
+++ src/main/scala/chess/Piece.scala
@@ -2,9 +2,10 @@ package strategygames.chess
 
 case class Piece(color: Color, role: Role) {
 
-  def is(c: Color)   = c == color
-  def is(r: Role)    = r == role
-  def isNot(r: Role) = r != role
+  def is(c: Color)    = c == color
+  def is(r: Role)     = r == role
+  def isNot(c: Color) = c != color
+  def isNot(r: Role)  = r != role
 
   def oneOf(rs: Set[Role]) = rs(role)
 
