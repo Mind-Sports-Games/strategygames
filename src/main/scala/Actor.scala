7ad58885692acfb1eb3a3075c1ef09a656c53838
Matt Tucker <matt.watkinsmedia.org>
2021-07-12 16:48:47 +0100

    Add Actor wrapper

---
+++ src/main/scala/Actor.scala
@@ -0,0 +1,34 @@
+package strategygames
+
+import format.Uci
+import scala.annotation.tailrec
+import scala.collection.mutable.ArrayBuffer
+
+sealed class Actor(
+    piece: Piece,
+    pos: Pos,
+    board: Board
+) {
+
+  def color: Color          = piece.color
+  def is(c: Color): Boolean = c == piece.color
+  def is(r: Role): Boolean  = r == piece.role
+  def is(p: Piece): Boolean = p == piece
+
+}
+
+object Actor {
+
+  final case class Chess(a: chess.Actor) extends Actor(
+    Piece.Chess(a.piece),
+    Pos.Chess(a.pos),
+    Board.Chess(a.board)
+  ) {}
+
+  final case class Draughts(a: draughts.Actor) extends Actor(
+    Piece.Draughts(a.piece),
+    Pos.Draughts(a.pos),
+    Board.Draughts(a.board)
+  ){}
+
+}
