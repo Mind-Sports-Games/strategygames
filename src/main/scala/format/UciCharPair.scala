1241269eb39a0f835b3adacad3ff77fe9020246a
Lakin Wecker <lakin@structuredabstraction.com>
2021-07-13 09:18:01 -0600

    Updating to match the UCI porting

--- src/main/scala/format/UciCharPair.scala
+++ src/main/scala/format/UciCharPair.scala
@@ -5,26 +5,21 @@ case class UciCharPair(a: Char, b: Char) {
 
 object UciCharPair {
 
-  def apply(lib: GameLib, uci: Uci): UciCharPair = (lib, uci) match {
-    case (GameLib.Draughts(), Uci.Draughts(uci)) => draughts.UciCharPair(uci)
-    case (GameLib.Chess(), Uci.Chess(uci))       => chess.UciCharPair(uci)
-    case _                                       => sys.error("Invalid match between game lib and underlying types")
+  def apply(lib: GameLib, uci: Uci): UciCharPair = lib match {
+    case GameLib.Draughts() => draughts.UciCharPair(uci)
+    case GameLib.Chess()       => chess.UciCharPair(uci)
   }
 
   // Unsure about these, probably will need them, but it's annoying to have such
   // specific methods for draughts. :(
   def apply(lib: GameLib, uci: Uci, ambiguity: Int): UciCharPair = (lib, uci) match {
-    case (GameLib.Draughts(), Uci.Draughts(uci)) => draughts.UciCharPair(uci, ambiguity)
+    case GameLib.Draughts() => draughts.UciCharPair(uci, ambiguity)
     case _                                       => sys.error("This method is only implemented for draughts")
   }
-  def apply(lib: GameLib, orig: Char, ambiguity: Int): UciCharPair = lib match {
-    case GameLib.Draughts() => draughts.UciCharPair(orig, ambiguity)
-    case _                  => sys.error("This method is only implemented for draughts")
-  }
+  def apply(lib: GameLib.Draughts, orig: Char, ambiguity: Int): UciCharPair =
+    draughts.UciCharPair(orig, ambiguity)
 
-  def combine(lib: GameLib, uci1: Uci, uci2: Uci): UciCharPair = lib match {
-    case GameLib.Draughts() => draughts.UciCharPair(orig, uci1, uci2)
-    case _                  => sys.error("This method is only implemented for draughts")
-  }
+  def combine(lib: GameLib.Draughts, uci1: Uci, uci2: Uci): UciCharPair =
+     draughts.UciCharPair(orig, uci1, uci2)
 
 }
