4923a5c5382c61651e4ca945016f789e97f577c0
Lakin Wecker <lakin@structuredabstraction.com>
2021-07-24 13:45:50 -0600

    NOpe, sure didn't

---
+++ src/main/scala/format/pgn/parsingModel.scala
@@ -0,0 +1,68 @@
+package strategygames
+package format.pgn
+
+import cats.data.Validated
+
+case class Sans(value: List[San]) extends AnyVal
+
+object Sans {
+  val empty = Sans(Nil)
+}
+
+// Standard Algebraic Notation
+trait San {
+
+  def apply(
+      situation: Situation,
+      iteratedCapts: Boolean = false,
+      forbiddenUci: Option[List[String]] = None
+  ): Validated[String, strategygames.MoveOrDrop]
+
+  def metas: Metas
+
+  def withMetas(m: Metas): San
+
+  def withSuffixes(s: Suffixes): San = withMetas(metas withSuffixes s)
+
+  def withComments(s: List[String]): San = withMetas(metas withComments s)
+
+  def withVariations(s: List[Sans]): San = withMetas(metas withVariations s)
+
+  def mergeGlyphs(glyphs: Glyphs): San = withMetas(
+    metas.withGlyphs(metas.glyphs merge glyphs)
+  )
+}
+
+case class Metas(
+    check: Boolean,
+    checkmate: Boolean,
+    comments: List[String],
+    glyphs: Glyphs,
+    variations: List[Sans]
+) {
+
+  def withSuffixes(s: Suffixes) =
+    copy(
+      check = s.check,
+      checkmate = s.checkmate,
+      glyphs = s.glyphs
+    )
+
+  def withGlyphs(g: Glyphs) = copy(glyphs = g)
+
+  def withComments(c: List[String]) = copy(comments = c)
+
+  def withVariations(v: List[Sans]) = copy(variations = v)
+}
+
+object Metas {
+  val empty = Metas(check = false, checkmate = false, Nil, Glyphs.empty, Nil)
+}
+
+
+case class Suffixes(
+    check: Boolean,
+    checkmate: Boolean,
+    promotion: Option[PromotableRole],
+    glyphs: Glyphs
+)
