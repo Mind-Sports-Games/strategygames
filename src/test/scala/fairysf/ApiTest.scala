1120470fa119b70d40112bc5219713ca6c12b355
Matt Tucker <matt.watkinsmedia.org>
2021-10-26 18:39:52 +0100

    Add more tests

--- src/test/scala/fairysf/ApiTest.scala
+++ src/test/scala/fairysf/ApiTest.scala
@@ -12,6 +12,12 @@ class FairyStockfishApiTest extends Specification with ValidatedMatchers {
         variant.Shogi.initialFen.value
       ) must_== true
     }
+    "return confirm sufficient material for both sides" in {
+      Api.insufficientMaterial(
+        variant.Shogi.fairysfName.name,
+        variant.Shogi.initialFen.value
+      ) must_== (false, false)
+    }
   }
 
   "Shogi initial fen minus middle rank" should {
@@ -36,23 +42,94 @@ class FairyStockfishApiTest extends Specification with ValidatedMatchers {
     "be game end" in {
       Api.gameResult(
         "chess",
-        //https://www.pychess.org/cdRztJdY?ply=74
         "rnb1kbnr/pppp1ppp/8/4p3/5PPq/8/PPPPP2P/RNBQKBNR w KQkq - 1 3",
       ) must_== GameResult.Checkmate()
       Api.gameResult(
         "chess",
-        //https://www.pychess.org/cdRztJdY?ply=74
         "r1bqkbnr/1ppppQ1p/p1n3p1/8/2B1P3/8/PPPP1PPP/RNB1K1NR b KQkq - 0 4",
       ) must_== GameResult.Checkmate()
     }
   }
+
   "Shogi Checkmate FEN" should {
+    //https://www.pychess.org/cdRztJdY?ply=74
+    val checkmateFen = "l2g1g1nl/5sk2/3p1p1p1/p3p1p1p/1n2n4/P4PP1P/1P1sPK1P1/5sR1+r/L4+p1N1[GPSBBglpp] w - - 4 38"
+    "be game end" in {
+      Api.gameResult(
+        variant.Shogi.fairysfName.name,
+        checkmateFen
+      ) must_== GameResult.Checkmate()
+    }
     "have no legal moves" in {
       Api.legalMoves(
         variant.Shogi.fairysfName.name,
-        //https://www.pychess.org/cdRztJdY?ply=74
-        "l2g1g1nl/5sk2/3p1p1p1/p3p1p1p/1n2n4/P4PP1P/1P1sPK1P1/5sR1+r/L4+p1N1[GPSBBglpp] w - - 4 38",
+        checkmateFen
       ).size must_== 0L
+    }
+  }
+
+  "Shogi fools mate moves" should {
+    val foolsFEN = "lnsg1gsnl/5rkb1/ppppppp+Pp/9/9/9/PPPPPPP1P/1B5R1/LNSGKGSNL[P] b - - 0 4"
+    "produce fools mate fen" in {
+      Api.fenFromMoves(
+        variant.Shogi.fairysfName.name,
+        variant.Shogi.initialFen.value,
+        Some(List("h3h4", "e9f8", "h4h5", "f8g8", "h5h6", "b8f8", "h6h7+"))
+      ).value must_== foolsFEN
+    }
+    "be checkmate" in {
+      Api.gameResult(
+        variant.Shogi.fairysfName.name,
+        foolsFEN,
+      ) must_== GameResult.Checkmate()
+    }
+    "have no legal moves" in {
+      Api.legalMoves(
+        variant.Shogi.fairysfName.name,
+        foolsFEN,
+      ).size must_== 0L
+    }
+  }
+
+  "Shogi stalemate is win" should {
+    val stalemateFEN = "8l/8k/9/8P/9/2P6/PP1PPPP2/1B5R1/LNSGKGSNL[] b - - 0 2"
+    "be a valid fen" in {
+      Api.validateFEN(
+        variant.Shogi.fairysfName.name,
+        stalemateFEN
+      ) must_== true
+    }
+    "have no legal moves" in {
+      Api.legalMoves(
+        variant.Shogi.fairysfName.name,
+        stalemateFEN,
+      ).size must_== 0L
+    }
+    "be stalemate" in {
+      Api.gameResult(
+        variant.Shogi.fairysfName.name,
+        stalemateFEN,
+      ) must_== GameResult.Checkmate()
+    }
+  }
+
+  "Chess king only" should {
+    val insufficientMaterialFEN = "4k3/8/8/8/8/8/PPPPPPPP/RNBQKBNR w KQ - 0 1"
+    "have insufficient material" in {
+      Api.insufficientMaterial(
+        "chess",
+        insufficientMaterialFEN
+      ) must_== (false, true)
+    }
+  }
+
+  "Shogi king only" should {
+    val insufficientMaterialFEN = "8k/9/9/9/9/9/PPPPPPPPP/1B5R1/LNSGKGSNL[LNSGGSNLBRPPPPPPPPP] b - - 0 2"
+    "have insufficient material" in {
+      Api.insufficientMaterial(
+        variant.Shogi.fairysfName.name,
+        insufficientMaterialFEN
+      ) must_== (false, true)
     }
   }
 
