d4405e1ea7414d8811c83a4da7871cc22911fbf5
Matt Tucker <matt.watkinsmedia.org>
2021-12-09 11:46:19 +0000

    Fix binary storage for fairysf and make more efficient and better for extension

--- src/main/scala/fairysf/format/pgn/Binary.scala
+++ src/main/scala/fairysf/format/pgn/Binary.scala
@@ -8,7 +8,9 @@ import scala.util.Try
 
 object Binary {
 
-  def writeMove(gf: GameFamily, m: String)             = Try(Writer.move(gf, m))
+  //writeMove only used in tests for chess/draughts
+  //would need to reconsider how we do this when we write gameFamily at the start of the game
+  //def writeMove(gf: GameFamily, m: String)             = Try(Writer.move(gf, m))
   def writeMoves(gf: GameFamily, ms: Iterable[String]) = Try(Writer.moves(gf, ms))
 
   def readMoves(bs: List[Byte])          = Try(Reader moves bs)
@@ -19,66 +21,51 @@ object Binary {
     val Drop = 1
   }
 
-  //private object Encoding {
-  //  val pieceInts: Map[String, Int] =
-  //    Map("K" -> 1, "Q" -> 2, "R" -> 3, "N" -> 4, "B" -> 5, "O-O" -> 6, "O-O-O" -> 7, "L" -> 8)
-  //  val pieceStrs: Map[Int, String]     = pieceInts map { case (k, v) => v -> k }
-  //  val dropPieceInts: Map[String, Int] = Map("P" -> 1, "Q" -> 2, "R" -> 3, "N" -> 4, "B" -> 5)
-  //  val dropPieceStrs: Map[Int, String] = dropPieceInts map { case (k, v) => v -> k }
-  //  val promotionInts: Map[String, Int] = Map("" -> 0, "Q" -> 1, "R" -> 2, "N" -> 3, "B" -> 4, "K" -> 6)
-  //  val promotionStrs: Map[Int, String] = promotionInts map { case (k, v) => v -> k }
-  //  val checkInts: Map[String, Int]     = Map("" -> 0, "+" -> 1, "#" -> 2)
-  //  val checkStrs: Map[Int, String]     = checkInts map { case (k, v) => v -> k }
-  //}
-
   private object Reader {
 
     private val maxPlies = 600
 
     def moves(bs: List[Byte]): List[String]          = moves(bs, maxPlies)
-    def moves(bs: List[Byte], nb: Int): List[String] = intMoves(bs map toInt, nb)
+    def moves(bs: List[Byte], nb: Int): List[String] = intMoves(bs map toInt, nb, None)
 
-    def intMoves(bs: List[Int], pliesToGo: Int): List[String] =
-      bs match {
-        case _ if pliesToGo <= 0 => Nil
-        case Nil                 => Nil
-        case b1 :: b2 :: b3 :: rest if moveType(b1) == MoveType.Move =>
-          moveUci(b1, b2, b3) :: intMoves(rest, pliesToGo - 1)
-        case b1 :: b2 :: rest if moveType(b1) == MoveType.Drop =>
-          dropUci(b1, b2) :: intMoves(rest, pliesToGo - 1)
-        case x => !!(x map showByte mkString ",")
+    def intMoves(bs: List[Int], pliesToGo: Int, gf: Option[GameFamily]): List[String] =
+      (bs, gf) match {
+        case (_, _) if pliesToGo <= 0 => Nil
+        case (Nil, _)                 => Nil
+        case (b1 :: rest, None)       => intMoves(rest, pliesToGo, Some(GameFamily(b1)))
+        case (b1 :: b2 :: rest, Some(gf)) if headerBit(b1) == MoveType.Move =>
+          moveUci(b1, b2) :: intMoves(rest, pliesToGo - 1, Some(gf))
+        case (b1 :: b2 :: rest, Some(gf)) if headerBit(b1) == MoveType.Drop =>
+          dropUci(gf, b1, b2) :: intMoves(rest, pliesToGo - 1, Some(gf))
+        case (x, _) => !!(x map showByte mkString ",")
       }
 
-    // 1 movetype
+    // 1 movetype (move or drop)
     // 7 pos (from)
     // ----
-    // 1 gamefamily (might need more than 1 bit once there are more than shogi/xiangqi)
+    // 1 promotion (bool)
+    // 7 pos (dest)
+    def moveUci(b1: Int, b2: Int): String =
+      s"${posFromInt(b1)}${posFromInt(b2)}${promotionFromInt(b2)}"
+
+    // 1 movetype
     // 7 pos (dest)
     // ----
     // 8 piece (only needs 4 bits?)
-    def moveUci(b1: Int, b2: Int, b3: Int): String =
-      s"${posFromInt(b1)}${posFromInt(b2)}${optionalPieceFromInt(gameFamily(b2), b3)}"
-
-    // 1 movetype
-    // 7 piece (only needs 4 bits?)
-    // ----
-    // 1 gamefamily
-    // 7 pos (dest)
-    def dropUci(b1: Int, b2: Int): String =
-      s"${pieceFromInt(gameFamily(b2), b1)}@${posFromInt(b2)}"
+    def dropUci(gf: GameFamily, b1: Int, b2: Int): String =
+      s"${pieceFromInt(gf, b2)}@${posFromInt(b1)}"
 
     def posFromInt(b: Int): String = Pos(right(b, 7)).get.toString()
 
-    def pieceFromInt(gf: Int, b: Int): String =
-      Role.allByBinaryInt(GameFamily(gf)).get(right(b, 7)).get.forsyth.toString
-
-    def optionalPieceFromInt(gf: Int, b: Int): String = b match {
-      case 0 => ""
-      case _ => pieceFromInt(gf, b)
+    def promotionFromInt(b: Int): String = headerBit(b) match {
+      case 1 => "+"
+      case _ => ""
     }
 
-    private def moveType(i: Int)   = i >> 7
-    private def gameFamily(i: Int) = (i >> 7) + 3
+    def pieceFromInt(gf: GameFamily, b: Int): String =
+      Role.allByBinaryInt(gf).get(right(b, 7)).get.forsyth.toString
+
+    private def headerBit(i: Int) = i >> 7
     //private def posString(i: Int) = fileChar(i >> 3).toString + rankChar(right(i, 3))
     //private def fileChar(i: Int)  = (i + 97).toChar
     //private def rankChar(i: Int)  = (i + 49).toChar
@@ -95,35 +82,29 @@ object Binary {
 
     def move(gf: GameFamily, str: String): List[Byte] =
       (str match {
-        case Uci.Move.moveR(src, dst, promotion) => moveUci(gf, src, dst, promotion)
+        case Uci.Move.moveR(src, dst, promotion) => moveUci(src, dst, promotion)
         case Uci.Drop.dropR(piece, dst) => dropUci(gf, piece, dst)
         case _ => sys.error(s"Invalid move to write: ${str}")
       }) map (_.toByte)
 
     def moves(gf: GameFamily, strs: Iterable[String]): Array[Byte] =
-      strs.flatMap(move(gf, _)).to(Array)
+      (gf.id.toByte :: strs.toList.flatMap(move(gf, _))).to(Array)
 
-    def moveUci(gf: GameFamily, src: String, dst: String, promotion: String) = List(
-      (moveType(MoveType.Move)) + Pos.fromKey(src).get.index,
-      (gameFamily(gf)) + Pos.fromKey(dst).get.index,
-      promotion.headOption match {
-        case Some(p) => Role.promotable(gf, p).map(_.binaryInt).getOrElse(0)
+    def moveUci(src: String, dst: String, promotion: String) = List(
+      (headerBit(MoveType.Move)) + Pos.fromKey(src).get.index,
+      (headerBit(promotion.headOption match {
+        case Some(_) => 1
         case None => 0
-      }
+      })) + Pos.fromKey(dst).get.index
     )
 
     def dropUci(gf: GameFamily, piece: String, dst: String) = List(
-      (moveType(MoveType.Drop)) + Role.allByForsyth(gf).get(piece(0)).get.binaryInt,
-      (gameFamily(gf)) + Pos.fromKey(dst).get.index
+      (headerBit(MoveType.Drop)) + Pos.fromKey(dst).get.index,
+      Role.allByForsyth(gf).get(piece(0)).get.binaryInt
     )
 
-    private def moveType(mt: Int)          = mt << 7
-    private def gameFamily(gf: GameFamily) = (gf.id - 3) << 7
+    private def headerBit(i: Int) = i << 7
 
-    //val pieceR       = "([KQRNBL])"
-    //val posR         = "([a-i][1-9]|[a-i]10)"
-    //val MoveR        = s"^$posR$posR$$".r
-    //val DropR        = s"^([QRNBP])@$posR$checkR$$".r
   }
 
   @inline private def toInt(b: Byte): Int = b & 0xff
