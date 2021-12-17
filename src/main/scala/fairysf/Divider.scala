7fe92bb31b70effb71c4b24b3a43dcfb2dfd1bcb
Matt Tucker <matt.watkinsmedia.org>
2021-12-17 14:46:15 +0000

    Remove unused fairysf classes (Hash, History)

--- src/main/scala/fairysf/Divider.scala
+++ src/main/scala/fairysf/Divider.scala
@@ -1,11 +1,10 @@
 package strategygames.fairysf
 
-import strategygames.{ Color, Division }
-
-import cats.syntax.option.none
+import strategygames.{ Division }
 
 object Divider {
 
-  def apply(boards: List[Board]): Division = Division.empty //TODO: ???
+  //TODO set this
+  def apply(boards: List[Board]): Division = Division.empty
 
 }
--- src/main/scala/fairysf/History.scala
+++ src/main/scala/fairysf/History.scala
@@ -1,38 +1,9 @@
 package strategygames.fairysf
 
-import strategygames.Color
-
 import format.Uci
 
 case class History(
     lastMove: Option[Uci] = None,
     positionHashes: PositionHash = Array.empty,
     halfMoveClock: Int = 0
-) {
-  private def isRepetition(times: Int) =
-    positionHashes.length > (times - 1) * 4 * Hash.size && {
-      // compare only hashes for positions with the same side to move
-      val positions = positionHashes.sliding(Hash.size, 2 * Hash.size).toList
-      positions.headOption match {
-        case Some(Array(x, y, z)) =>
-          (positions count {
-            case Array(x2, y2, z2) => x == x2 && y == y2 && z == z2
-            case _                 => false
-          }) >= times
-        case _ => times <= 1
-      }
-    }
-
-  def threefoldRepetition = isRepetition(3)
-
-  def fivefoldRepetition = isRepetition(5)
-
-  override def toString = {
-    val positions = (positionHashes grouped Hash.size).toList
-    s"${lastMove.fold("-")(_.uci)} ${positions.map(Hash.debug).mkString(" ")}"
-  }
-}
-
-object History {
-
-}
+)
