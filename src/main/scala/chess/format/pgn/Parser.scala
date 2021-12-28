820c7f5acaf3534d87c2410b9a5abb38e6b94553
Matt Tucker <matt.watkinsmedia.org>
2021-12-28 19:46:04 +0000

    Fix TagParser to allow P1/P2

--- src/main/scala/chess/format/pgn/Parser.scala
+++ src/main/scala/chess/format/pgn/Parser.scala
@@ -39,7 +39,7 @@ object Parser {
         splitted <- splitTagAndMoves(preprocessed)
         tagStr  = splitted._1
         moveStr = splitted._2
-        preTags     <- TagParser(tagStr)
+        preTags     <- TagParser(tagStr.pp("tagStr"))
         parsedMoves <- MovesParser(moveStr)
         init         = parsedMoves._1
         strMoves     = parsedMoves._2
@@ -380,7 +380,7 @@ object Parser {
         }
       }
 
-    val tagName: Parser[String] = "[" ~> """[a-zA-Z]+""".r
+    val tagName: Parser[String] = "[" ~> """[a-zA-Z12]+""".r
 
     val tagValue: Parser[String] = """"(?:[^"\\]|\\.)*"""".r <~ "]" ^^ {
       _.stripPrefix("\"").stripSuffix("\"").replace("\\\"", "\"")
