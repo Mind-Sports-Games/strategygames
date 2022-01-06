c795d8b0588b97448ee6a42165b4263e59901904
James Heppell <jheps@DESKTOP-IOQBCIN.localdomain>
2022-01-06 09:59:49 +0000

    make 5 check a non aivariant

--- src/main/scala/chess/variant/FiveCheck.scala
+++ src/main/scala/chess/variant/FiveCheck.scala
@@ -19,6 +19,7 @@ case object FiveCheck
   def perfId: Int    = 15
   def perfIcon: Char = '.'
 
+  override def aiVariant: Boolean   = false
   override val initialFen = FEN("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1 +0+0")
 
   override def finalizeBoard(board: Board, uci: format.Uci, capture: Option[Piece]): Board =
