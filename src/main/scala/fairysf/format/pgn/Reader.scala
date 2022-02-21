5fdcc288e407fe02ddcda134ce2731322866191e
Lakin Wecker <lakin.wecker@gmail.com>
2022-02-21 08:36:22 -0700

    Removing superfluous .apply

--- src/main/scala/fairysf/format/pgn/Reader.scala
+++ src/main/scala/fairysf/format/pgn/Reader.scala
@@ -29,6 +29,22 @@ object Reader {
   def moves(moveStrs: Iterable[String], tags: Tags): Validated[String, Result] =
     movesWithSans(moveStrs, identity, tags)
 
+  // Because fairysf deals exclusively with UCI moves, we need this version
+  // for parsing replaces from uci move
+  def uciMoves(gf: GameFamily, moveStrs: Iterable[String], tags: Tags): Validated[String, Result] = {
+    val moves = moveStrs.pp("Moves").flatMap(Uci(gf, _)).pp("flatMoves")
+    if (moves.size.pp("newSize") < moveStrs.size.pp("oldSize")) {
+      Validated.invalid("Invalid UCI moves")
+    } else {
+      Validated.valid(
+        makeReplayFromUCI(
+          makeGame(tags),
+          moves.toList
+        )
+      )
+    }
+  }
+
   def fullWithSans(pgn: String, op: Sans => Sans, tags: Tags = Tags.empty): Validated[String, Result] =
     Parser.full(cleanUserInput(pgn)) map { parsed =>
       makeReplay(makeGame(parsed.tags ++ tags), op(parsed.sans))
@@ -54,6 +70,16 @@ object Reader {
         san(StratSituation.wrap(replay.state.situation)).fold(
           err => Result.Incomplete(replay, err),
           move => Result.Complete(replay addMove StratMove.toFairySF(move))
+        )
+      case (r: Result.Incomplete, _) => r
+    }
+
+  private def makeReplayFromUCI(game: Game, ucis: List[Uci]): Result =
+    ucis.foldLeft[Result](Result.Complete(Replay(game))) {
+      case (Result.Complete(replay), uci) =>
+        uci(replay.state.situation).fold(
+          err => Result.Incomplete(replay, err),
+          move => Result.Complete(replay addMove move)
         )
       case (r: Result.Incomplete, _) => r
     }
