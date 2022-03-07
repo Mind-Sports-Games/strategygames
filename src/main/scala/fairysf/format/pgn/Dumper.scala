011717844eb14bd8d1f1363c4dc824a3f13d1f64
Lakin Wecker <lakin.wecker@gmail.com>
2022-03-06 18:18:58 -0700

    Implementing the Dumper with just raw UCI

--- src/main/scala/fairysf/format/pgn/Dumper.scala
+++ src/main/scala/fairysf/format/pgn/Dumper.scala
@@ -3,11 +3,20 @@ package format.pgn
 
 object Dumper {
 
-  def apply(situation: Situation, data: strategygames.fairysf.Move, next: Situation): String = "" //TODO: ???
+  def apply(situation: Situation, data: strategygames.fairysf.Move, next: Situation): String = data.toUci.lilaUci
 
-  def apply(data: strategygames.fairysf.Drop, next: Situation): String = "" //TODO: ???
+  def apply(data: strategygames.fairysf.Drop, next: Situation): String = data.toUci.lilaUci
 
-  def apply(data: strategygames.fairysf.Move): String = "" //TODO: ???
+  def apply(data: strategygames.fairysf.Move): String =
+    apply(
+      data.situationBefore,
+      data,
+      data.finalizeAfter situationOf !data.player
+    )
 
-  def apply(data: strategygames.fairysf.Drop): String = "" //TODO: ???
+  def apply(data: strategygames.fairysf.Drop): String =
+    apply(
+      data,
+      data.finalizeAfter situationOf !data.player
+    )
 }
