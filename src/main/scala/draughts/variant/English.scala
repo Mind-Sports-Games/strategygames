dadd91d53ea5d75570fa4b7d69447519f70dca77
Matt Tucker <matt.watkinsmedia.org>
2022-11-08 08:04:17 +0000

    Corrections for English draughts. Flip start color

--- src/main/scala/draughts/variant/English.scala
+++ src/main/scala/draughts/variant/English.scala
@@ -3,7 +3,6 @@ package variant
 
 import strategygames.Player
 
-import scala.annotation.tailrec
 import cats.implicits._
 
 case object English
@@ -24,8 +23,12 @@ case object English
   def initialFen          = Russian.initialFen
   def startingPosition    = Russian.startingPosition
 
+  override val openingTables = List(OpeningTable.tableACF11ManBallot)
+
   override def playerNames: Map[Player, String]  = Map(P1 -> "Black", P2 -> "White")
   override def playerColors: Map[Player, String] = Map(P1 -> "black", P2 -> "white")
+
+  override def invertNumericCoords: Boolean = true
 
   override def flyingKings: Boolean = false
 
@@ -118,50 +121,29 @@ case object English
       captured: Option[List[Piece]],
       situationBefore: Situation,
       finalSquare: Boolean
-  ): Board = {
-    val remainingCaptures =
-      if (finalSquare) 0 else situationBefore.captureLengthFrom(uci.orig).getOrElse(0) - 1
-    if (remainingCaptures > 0) board
-    else {
-      val p1Actors                                                       = board.actorsOf(Player.P1)
-      val p2Actors                                                       = board.actorsOf(Player.P2)
-      val p1Kings                                                        = p1Actors.count(_.piece is King)
-      val p2Kings                                                        = p2Actors.count(_.piece is King)
-      val p1Pieces                                                       = p1Actors.size
-      val p2Pieces                                                       = p2Actors.size
-      def loneKing(strongPieces: Int, strongKings: Int, weakKing: Actor) =
-        strongPieces == 3 && strongKings >= 1 && weakKing.onLongDiagonal && board.piecesOnLongDiagonal == 1
-      val p1LoneKing                                                     =
-        if (p1Kings == 1 && p1Pieces == 1 && p2Kings >= 1) {
-          loneKing(p2Pieces, p2Kings, p1Actors.head)
-        } else false
-      val p2LoneKing                                                     =
-        if (p2Kings == 1 && p2Pieces == 1 && p1Kings >= 1) {
-          loneKing(p1Pieces, p1Kings, p2Actors.head)
-        } else false
-      if (p1LoneKing || p2LoneKing) {
-        board updateHistory { h =>
-          // "abuse" kingmove counter to count the amount of moves made on the long
-          // diagonal by the side with a lone king against 3 (see 7.2.7)
-          h.withKingMove(Player(p1LoneKing), None, true)
-        } withoutGhosts
-      } else board.withoutGhosts
-    }
-  }
+  ): Board = Pool.finalizeBoard(board, uci, captured, situationBefore, finalSquare)
 
-  def maxDrawingMoves(board: Board): Option[Int] = Russian.maxDrawingMoves(board)
+  def maxDrawingMoves(board: Board): Option[Int] = drawingMoves
+
+  // Only auto draw rule for American is 40 moves with no progress
+  private def drawingMoves: Option[Int] = Some(80)
+
   def updatePositionHashes(
       board: Board,
       move: Move,
       hash: strategygames.draughts.PositionHash
-  ): PositionHash =
-    Russian.updatePositionHashes(board, move, hash)
-
-  override def validSide(board: Board, strict: Boolean)(player: Player) = {
-    val roles = board rolesOf player
-    (roles.count(_ == Man) > 0 || roles.count(_ == King) > 0) &&
-    (!strict || roles.size <= 12) &&
-    (!menOnPromotionRank(board, player) || board.ghosts != 0)
+  ): PositionHash = {
+    val newHash = Hash(Situation(board, !move.piece.player))
+    drawingMoves match {
+      case Some(_) =>
+        if (move.captures || move.promotes)
+          newHash // reset hash on capture or promotion
+        else newHash ++ hash
+      case _       => newHash
+    }
   }
+
+  override def validSide(board: Board, strict: Boolean)(player: Player) =
+    Russian.validSide(board, strict)(player)
 
 }
