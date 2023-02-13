edbb9357bfba3cd2b0f915d19f88f635f0b3e49a
James Heppell <jheps@DESKTOP-IOQBCIN.localdomain>
2023-02-13 17:13:15 +0000

    add initial amazons tests

--- src/test/scala/fairysf/AmazonsVariantTest.scala
+++ src/test/scala/fairysf/AmazonsVariantTest.scala
@@ -7,15 +7,15 @@ class AmazonsVariantTest extends FairySFTest {
 
   // https://playstrategy.dev/4eCFkjwt
   val amazonsGame = List(
-    "d1d6pg9",
-    "d10f10pe10",
-    "a4f9pe9",
-    "a7a10pb10",
-    "d6a9pb9",
-    "j7j10ph10",
-    "j4j9ph9",
-    "j10i10pi9",
-    "j9j10pj9" // then p2 has no moves
+    "d1d6,d6g9",
+    "d10f10,f10e10",
+    "a4f9,f9e9",
+    "a7a10,a10b10",
+    "d6a9,a9b9",
+    "j7j10,j10h10",
+    "j4j9,j9h9",
+    "j10i10,i10i9",
+    "j9j10,j10j9" // then p2 has no moves
   )
 
   "Amazons" should {
@@ -33,59 +33,29 @@ class AmazonsVariantTest extends FairySFTest {
       }
     }
 
-    "play d1d6pc8 and opponent have moves" in {
-      val position  = Api.positionFromVariant(variant.Amazons)
-      val position2 = position.makeMoves(List("d1d6pc8"))
-      //   println("Possible moves in this position")
-      //   position2.legalMoves.map(println(_))
-
-      //   println("current fen")
-      //   println(position2.fen)
-
-      //   println(position2.legalMoves.size)
-
-      position2.legalMoves.size > 0
-
+    "2176 move from start pos" in {
+      val position = Api.positionFromVariant(variant.Amazons)
+      position.legalMoves.size must_== 2176
     }
 
     "have 32 pawn drops from starting d1d6 move" in {
       val position = Api.positionFromVariant(variant.Amazons)
-
-      //   println("Possible moves in this position")
-      //   position.legalMoves.filter(x => x.contains("d1d6")).map(println(_))
-
-      //   println("current fen")
-      //   println(position.fen)
-
       position.legalMoves.filter(x => x.contains("d1d6")).size must_== 32
     }
 
-    "have openning move d1d6pg9" in {
+    "have openning move d1d6,d6g9" in {
       val position = Api.positionFromVariant(variant.Amazons)
-      // val position2 = position.makeMoves(amazonsGame.take(1))
-
-      //   println("Possible moves in this position")
-      //   position2.legalMoves.map(println(_))
-
-      //   println("current fen")
-      //   println(position2.fen)
-
-      position.legalMoves.contains("d1d6pg9")
+      position.legalMoves.contains("d1d6,d6g9")
     }
 
-    // "P1 win in example game" in {
-    //   val position  = Api.positionFromVariant(variant.Amazons)
-    //   val position2 = position.makeMoves(amazonsGame)
-
-    //   //   println("Possible moves in this position")
-    //   //   position2.legalMoves.map(println(_))
-
-    //   //   println("current fen")
-    //   //   println(position2.fen)
-
-    //   position2.legalMoves.size must_== 0
-    //   position2.gameEnd must_== true
-    // }
+    "P1 win in example game" in {
+      val position  = Api.positionFromVariant(variant.Amazons)
+      val position2 = position.makeMoves(amazonsGame)
+      println("current fen")
+      println(position2.fen)
+      position2.legalMoves.size must_== 0
+      position2.gameEnd must_== true
+    }
 
     "Should detect game end from fen" in {
       val position = FEN(
@@ -101,12 +71,13 @@ class AmazonsVariantTest extends FairySFTest {
 
     "Have 25 moves from position" in {
       val fen      = FEN(
-        "qp2pqqpq1/QP2PQPPp1/10/10/10/10/10/10/10/6Q2Q[PPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPpppppppppppppppppppppppppppppppppppppppppp] b - - 9 5"
+        "q*2*qq*q1/Q*2*Q***1/10/10/10/10/10/10/10/6Q2Q[PPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPpppppppppppppppppppppppppppppppppppppppppp] b - - 9 5"
       )
       val position = Api.positionFromVariantNameAndFEN(variant.Amazons.key, fen.toString())
-      position.legalMoves.map(println(_))
-      position.legalMoves.size must_== 25
-
+      println("current fen")
+      println(position.fen)
+      // position.legalMoves.map(println(_))
+      position.legalMoves.size must_== 26
     }
 
   }
