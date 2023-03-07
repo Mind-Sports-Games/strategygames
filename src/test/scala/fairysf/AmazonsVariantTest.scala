ba1ee6243fa080e4da0e16f5568eee9026cd8d2d
Matt Tucker <matt.watkinsmedia.org>
2023-03-07 08:58:04 +0000

    Add Amazon Variant tests

--- src/test/scala/fairysf/AmazonsVariantTest.scala
+++ src/test/scala/fairysf/AmazonsVariantTest.scala
@@ -3,35 +3,31 @@ package strategygames.fairysf
 import variant.Amazons
 import strategygames.fairysf.format.FEN
 
+import strategygames.{ Player, Status }
+
 class AmazonsVariantTest extends FairySFTest {
 
   val initialFen = FEN(
     "3q2q3/10/10/q8q/10/10/Q8Q/10/10/3Q2Q3[PPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPpppppppppppppppppppppppppppppppppppppppppppppp] w - - 0 1"
   )
 
-  val amazonsGame = List(
-    "d1d6,d6g9",
-    "d10f10,f10e10",
-    "a4f9,f9e9",
-    "a7a10,a10b10",
-    "d6a9,a9b9",
-    "j7j10,j10h10",
-    "j4j9,j9h9",
-    "j10i10,i10i9",
-    "j9j10,j10j9" // then p2 has no moves
-  )
-
-  val amazonsGame2 = List(
-    "a4a2,a2b2",
-    "a7a5,a5b5",
-    "j4a4,a4b4",
-    "d10d7,d7a10",
-    "d1a1,a1b1",
-    "g10g9,g9g10",
-    "g1e3,e3j3",
-    "j7i7,i7i10",
-    "e3a3,a3b3" // most p2 moves will end game and
-    // then p1 has no moves
+  val amazonsGame = Vector(
+    "d1d6",
+    "P@g9",
+    "d10f10",
+    "P@e10",
+    "a4f9",
+    "P@e9",
+    "a7a10",
+    "P@b10",
+    "d6a9",
+    "P@b9",
+    "j7j10",
+    "P@h10",
+    "j4j9",
+    "P@h9",
+    "j10i10",
+    "P@i9" // then after j9j10,P@j9 p2 has no moves
   )
 
   "Amazons" should {
@@ -40,6 +36,9 @@ class AmazonsVariantTest extends FairySFTest {
       val game = fenToGame(initialFen, Amazons)
       game must beValid.like {
         case game => {
+          game.situation.end must beFalse
+          // game.situation.playable(false).pp("playable") must beTrue
+          game.situation.status == None must beTrue
           game.situation.winner == None must beTrue
         }
       }
@@ -48,99 +47,28 @@ class AmazonsVariantTest extends FairySFTest {
     "80 move from start pos" in {
       // val game = fenToGame(initialFen, Amazons)
       val game = Game(Amazons)
-      game.situation.pp("sit").moves.pp("moves").size must_== 4
+      game.situation.moves.size must_== 4
       game.situation.moves.toList(0)._2.size must_== 20
-      // strategygames.fairysf.Game.apply(strategygames.fairysf.variant.Amazons).situation.moves.toList(0)._2.size
+      game.situation.moves.toList.map(_._2.size).sum must_== 80
+      // strategygames.fairysf.Game.apply(strategygames.fairysf.variant.Amazons).situation.moves.toList.map(_._2.size).sum
     }
 
     "P1 win in example game" in {
-      val position  = Api.positionFromVariant(variant.Amazons)
-      val position2 = position.makeMoves(amazonsGame)
-      position2.legalMoves.size must_== 0
-      position2.gameEnd must_== true
-    }
-
-    "P2 win in example game" in {
-      val position  = Api.positionFromVariant(variant.Amazons)
-      val position2 = position.makeMoves(amazonsGame2)
-      position2.legalMoves.size > 0
-      position2.gameEnd must_== false
-      val position3 = position2.makeMoves(List("a5a9,a9a5"))
-      position3.gameEnd must_== true
-      val position4 = position2.makeMoves(List("a5a9,a9a8"))
-      position4.gameEnd must_== false
-    }
-
-    "Should detect game end from fen" in {
-      val position = FEN(
-        "qp2pqqpqQ/QP2PQPPpP/10/10/10/10/10/10/10/6Q3[PPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPpppppppppppppppppppppppppppppppppppppppppp] b - - 9 5"
+      val replay  = Replay.gameMoveWhileValid(amazonsGame, initialFen, variant.Amazons)
+      val game    = replay._2.last._1
+      game.situation.moves.keys.size must_== 4
+      game.situation.moves.keys.filter(_ == Pos.J9).size must_== 1
+      game.situation.moves(Pos.J9).filter(_.dest == Pos.J10).size must_== 1
+      val game2   = game.apply(game.situation.moves(Pos.J9).filter(_.dest == Pos.J10)(0))
+      game2.situation.dropsAsDrops.size must_== 9
+      game2.situation.dropsAsDrops.filter(_.pos.key == "j9").size must_== 1
+      val gameEnd = game2.applyDrop(game2.situation.dropsAsDrops.filter(_.pos.key == "j9")(0))
+      gameEnd.situation.board.variant.exportBoardFen(game.situation.board) must_== FEN(
+        "qp2pqqpq1/QP2PQPPpQ/10/10/10/10/10/10/10/6Q3[PPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPpppppppppppppppppppppppppppppppppppppppppppppp] w - - 8 5"
       )
-      val game     = fenToGame(position, Amazons)
-      game must beValid.like {
-        case game => {
-          game.situation.playable(false) must beFalse
-        }
-      }
-    }
-
-    "Have 25 moves from position" in {
-      val fen      = FEN(
-        "q*2*qq*q1/Q*2*Q***1/10/10/10/10/10/10/10/6Q2Q[PPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPpppppppppppppppppppppppppppppppppppppppppp] b - - 9 5"
-      )
-      val position = Api.positionFromVariantNameAndFEN(variant.Amazons.key, fen.toString())
-      // position.legalMoves.map(println(_))
-      position.legalMoves.size must_== 26
-    }
-
-    "play moves in two amazon games" in {
-      val position  = Api.positionFromVariant(variant.Amazons)
-      val position2 = position.makeMoves(List("d1a1,a1a2"))
-      val position3 = position.makeMoves(List("g1g9,g9d9"))
-      position2.legalMoves.size > position3.legalMoves.size
-    }
-
-    "play moves in two different games" in {
-      val position  = Api.positionFromVariant(variant.Amazons)
-      val position2 = position.makeMoves(List("g1g9,g9d9"))
-
-      val positionShogi  = Api.positionFromVariant(variant.Shogi)
-      val positionShogi2 =
-        positionShogi.makeMoves(List("h3h4", "b7b6", "h4h5", "g7g6", "h2h4", "h8c3+", "b2c3"))
-      // positionShogi2.legalMoves.map(println(_))
-      val position3      = position2.makeMoves(List("j7e2,e2d2", "j4g7,g7b7"))
-
-      position3.legalMoves.size > positionShogi2.legalMoves.size
-    }
-
-    "play part of a game" in {
-      val position  = Api.positionFromVariant(variant.Amazons)
-      val position2 = position.makeMoves(
-        List(
-          "d1d4,d4d5",
-          "d10d7,d7d6",
-          "g1g4,g4g5",
-          "g10g7,g7g6",
-          "d4e4,e4e5",
-          "d7e7,e7e6",
-          "g4f4,f4f5",
-          "g7f7,f7f6",
-          "a4c4,c4c5",
-          "a7c7,c7c6",
-          "c4b4,b4b5",
-          "c7b7,b7b6",
-          "b4a4,a4a5",
-          "b7a7,a7a6",
-          "j4h4,h4h5",
-          "j7h7,h7h6",
-          "h4i4,i4i5",
-          "h7i7,i7i6",
-          "i4j4,j4j5",
-          "i7j7,j7j6"
-        )
-      )
-
-      position2.gameEnd must_== false
-      position2.fen.toString() must_== "10/10/10/q3qq3q/pppppppppp/pppppppppp/Q3QQ3Q/10/10/10 w - - 20 11"
+      gameEnd.situation.end must beTrue
+      gameEnd.situation.status must_== Some(Status.VariantEnd)
+      gameEnd.situation.winner must_== Some(Player.P1)
     }
 
   }
