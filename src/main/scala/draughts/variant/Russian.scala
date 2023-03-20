6cf5c6ff4d2744bf930878b78b6623b344ea2b55
Lakin Wecker <lakin@wecker.ca>
2023-03-19 18:36:16 -0600

    Apparently this fixes it? wut?

--- src/main/scala/draughts/variant/Russian.scala
+++ src/main/scala/draughts/variant/Russian.scala
@@ -23,7 +23,11 @@ case object Russian
   def perfIcon: Char = ''
 
   override def fenVariant    = true
-  val pieces                 = Variant.symmetricThreeRank(Vector(Man, Man, Man, Man), boardSize)
+  def pieces                 = {
+    println("Russian.pieces")
+    val pieces = Variant.symmetricThreeRank(Vector(Man, Man, Man, Man), boardSize)
+    pieces
+  }
   val initialFen             = FEN("W:W21,22,23,24,25,26,27,28,29,30,31,32:B1,2,3,4,5,6,7,8,9,10,11,12:H0:F1")
   val startingPosition       = StartingPosition("---", initialFen, "", "Initial position".some)
   override val openingTables = List(OpeningTable.tableFMJD, OpeningTable.tableIDFBasic)
