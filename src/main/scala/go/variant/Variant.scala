0d615d1cd6379f98dfacd2eb96fbb0f135c94f15
Lakin Wecker <lakin@wecker.ca>
2023-09-27 15:55:02 -0600

    make it new position

--- src/main/scala/go/variant/Variant.scala
+++ src/main/scala/go/variant/Variant.scala
@@ -113,8 +113,8 @@ abstract class Variant private[variant] (
                 situation.history.copy(
                   lastMove = Uci.Drop(Role.defaultRole, dest).some,
                   score = Score(
-                    oldPosition.fen.player1Score, // TODO: generating the scores is slow
-                    oldPosition.fen.player2Score  //        especially when we have to generate a FEN to get it
+                    newPosition.fen.player1Score, // TODO: generating the scores is slow
+                    newPosition.fen.player2Score  //        especially when we have to generate a FEN to get it
                   ),
                   captures = situation.history.captures.add(
                     situation.player,
