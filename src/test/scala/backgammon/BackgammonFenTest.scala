a0335619f0b5fd58da2c464e4ae4a30f8f6d5b43
Matt Tucker <matt.watkinsmedia.org>
2024-02-29 11:36:16 +0000

    Add Backgammon tests

--- src/test/scala/backgammon/BackgammonFenTest.scala
+++ src/test/scala/backgammon/BackgammonFenTest.scala
@@ -2,12 +2,14 @@ package strategygames.backgammon
 import strategygames.Player
 
 import org.specs2.matcher.ValidatedMatchers
-import org.specs2.mutable.Specification
 
-class BackgammonFenTest extends Specification with ValidatedMatchers {
+import strategygames.backgammon.format.{ FEN, Forsyth }
+import strategygames.backgammon.variant.Backgammon
+
+class BackgammonFenTest extends BackgammonTest with ValidatedMatchers {
 
   "Backgammon stoneArray from initial fen" should {
-    val fen    = variant.Backgammon.initialFen
+    val fen    = Backgammon.initialFen
     val pieces = fen.pieces
 
     "be valid" in {
@@ -54,14 +56,14 @@ class BackgammonFenTest extends Specification with ValidatedMatchers {
   }
 
   "Initial fen starting player" should {
-    val fen = variant.Backgammon.initialFen
+    val fen = Backgammon.initialFen
     "Starting player is white/p1" in {
       fen.player must_== Some(Player.P1)
     }
   }
 
   "fen 6,2s,2s,2s,2s,2s,2s/6,5S,5[2S] - - b 8 3 10" should {
-    val fen    = format.FEN("6,2s,2s,2s,2s,2s,2s/6,5S,5[2S] 5 6 b 8 3 10")
+    val fen    = FEN("6,2s,2s,2s,2s,2s,2s/6,5S,5[2S] 5 6 b 8 3 10")
     val pieces = fen.pieces
 
     "Player turn is black/p2" in {
@@ -110,6 +112,26 @@ class BackgammonFenTest extends Specification with ValidatedMatchers {
     "score" in {
       fen.player1Score must_== 8
       fen.player2Score must_== 3
+    }
+  }
+
+  "gameplay" should {
+    "match expected fen" in {
+      val actionStrs = List(
+        "1/5",
+        "l2k2",
+        "e1j1",
+        "endturn",
+        "2/4",
+        "l1j1",
+        "g2k2",
+        "endturn",
+        "2/1",
+        "s@k2"
+      )
+      playActionStrs(actionStrs) must beValid.like { g =>
+        Forsyth.>>(g) must_== FEN("5S,3,3s,1,4s,3,1S,1S/5s,3,2S,1,5S,2,1s,1,1s[1S,1s] 1 2 w 0 0 1")
+      }
     }
   }
 
