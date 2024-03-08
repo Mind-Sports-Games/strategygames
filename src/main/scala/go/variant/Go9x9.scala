aae10ecca272e9d3e5b19796d56e7cf018e25870
Matt Tucker <matt.watkinsmedia.org>
2024-03-08 00:18:17 +0000

    Change Go 9x9 Komi to 5.5

--- src/main/scala/go/variant/Go9x9.scala
+++ src/main/scala/go/variant/Go9x9.scala
@@ -20,7 +20,7 @@ case object Go9x9
 
   // cache this rather than checking with the API everytime
   override def initialFen =
-    format.FEN("9/9/9/9/9/9/9/9/9[SSSSSSSSSSssssssssss] b - 0 75 0 0 75 1")
+    format.FEN("9/9/9/9/9/9/9/9/9[SSSSSSSSSSssssssssss] b - 0 55 0 0 55 1")
 
   override def boardFenFromHandicap(handicap: Int): String = {
     handicap match {
