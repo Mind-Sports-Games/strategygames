e7026df027054755fffa308060bb11db4bdec452
Matt Tucker <matt.watkinsmedia.org>
2024-03-08 13:11:59 +0000

    Clear used dice when ending turn

--- src/main/scala/backgammon/Board.scala
+++ src/main/scala/backgammon/Board.scala
@@ -31,7 +31,7 @@ case class Board(
   lazy val unusedDiceStr: String = if (unusedDice.isEmpty) "-" else unusedDice.mkString("/")
 
   lazy val usedDice: List[Int] = diceToActionDice(
-    history.recentTurn.flatMap {
+    history.currentTurn.flatMap {
       case u: Uci.DiceRoll => Some(u.dice)
       case _               => None
     }.flatten
