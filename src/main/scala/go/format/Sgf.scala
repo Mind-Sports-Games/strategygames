f1af9d63eec039692d05f4431ecf8c1a61e8d4fe
JamesHeppell <jheppell100@hotmail.com>
2024-07-02 14:10:09 +0100

    fix sgf for go handicap games - add initial moves

--- src/main/scala/go/format/Sgf.scala
+++ src/main/scala/go/format/Sgf.scala
@@ -2,6 +2,7 @@ package strategygames.go.format
 
 import strategygames.ActionStrs
 import strategygames.go.variant.Variant
+import strategygames.Player
 
 object Sgf {
 
@@ -14,15 +15,33 @@ object Sgf {
       }
     }.toList
 
-  private def indexToPlayer(index: Int): Char = if (index % 2 == 0) 'B' else 'W'
+  private def initialMoves(variant: Variant, initialFen: FEN): List[(Char, String)] =
+    strategygames.go.Api
+      .pieceMapFromFen(variant.key, initialFen.value)
+      .toList
+      .map { case (pos, piece) =>
+        (if (piece.player == Player.P1) 'B' else 'W', pos.sgf(variant.boardSize.height))
+      }
+      .sortBy(_._2)
 
-  private def sgfPosWithPlayer(sgfPos: List[String]): List[(Char, String)] =
-    sgfPos.zipWithIndex.map { case (pos, index) => (indexToPlayer(index), pos) }
+  private def indexToPlayer(index: Int, isHandicapGame: Boolean): Char =
+    if ((index % 2 == 0 && !isHandicapGame) || (index % 2 == 1 && isHandicapGame)) 'B' else 'W'
+
+  private def sgfPosWithPlayer(sgfPos: List[String], isHandicapGame: Boolean): List[(Char, String)] =
+    sgfPos.zipWithIndex.map { case (pos, index) => (indexToPlayer(index, isHandicapGame), pos) }
 
   private def turnToString(posWithPlayer: (Char, String)): String =
     s";${posWithPlayer._1}[${posWithPlayer._2}]"
 
-  def actionStrsToOutput(variant: Variant, actionStrs: ActionStrs, d: Int = 0) =
-    sgfPosWithPlayer(actionStrsToSGFPos(variant, actionStrs)).map(turnToString).drop(d).mkString("\n")
+  def actionStrsToOutput(
+      variant: Variant,
+      actionStrs: ActionStrs,
+      initialFen: Option[FEN] = None,
+      d: Int = 0
+  ) =
+    (initialFen.fold(List.empty[(Char, String)])(f => initialMoves(variant, f)) ::: sgfPosWithPlayer(
+      actionStrsToSGFPos(variant, actionStrs),
+      initialFen.fold(false)(f => f.handicap.fold(false)(h => h > 0))
+    )).map(turnToString).drop(d).mkString("\n")
 
 }
