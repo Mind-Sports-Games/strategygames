a1105c2bf3db027a6cc95c0c5e5868389b0550f1
Matt Tucker <matt.watkinsmedia.org>
2024-12-12 15:58:31 +0000

    Change SGF Dumper to take into account start player colour

--- src/main/scala/backgammon/format/Sgf.scala
+++ src/main/scala/backgammon/format/Sgf.scala
@@ -1,6 +1,6 @@
 package strategygames.backgammon.format
 
-import strategygames.ActionStrs
+import strategygames.{ ActionStrs, Player }
 
 object Sgf {
 
@@ -18,15 +18,19 @@ object Sgf {
         .mkString("")
     }.toList
 
-  private def indexToPlayer(index: Int): Char = if (index % 2 == 0) 'W' else 'B'
+  private def indexToPlayer(index: Int, startPlayer: Player): Char =
+    if (index % 2 == startPlayer.hashCode - 1) 'W' else 'B'
 
-  private def sgfWithPlayer(sgf: List[String]): List[(Char, String)] =
-    sgf.zipWithIndex.map { case (notation, index) => (indexToPlayer(index), notation) }
+  private def sgfWithPlayer(sgf: List[String], startPlayer: Player): List[(Char, String)] =
+    sgf.zipWithIndex.map { case (notation, index) => (indexToPlayer(index, startPlayer), notation) }
 
   private def turnToString(turnWithPlayer: (Char, String)): String =
     s";${turnWithPlayer._1}[${turnWithPlayer._2}]"
 
-  def actionStrsToOutput(actionStrs: ActionStrs, d: Int = 0) =
-    sgfWithPlayer(actionStrsToSGF(actionStrs)).map(turnToString).drop(d).mkString("\n")
+  def actionStrsToOutput(actionStrs: ActionStrs, initialFen: Option[FEN], d: Int = 0) =
+    sgfWithPlayer(actionStrsToSGF(actionStrs), initialFen.flatMap(_.player).getOrElse(Player.P1))
+      .map(turnToString)
+      .drop(d)
+      .mkString("\n")
 
 }
