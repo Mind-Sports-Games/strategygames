ba01a5198812f5bc679f4e16f1383ea75c2cfe8a
Matt Tucker <matt.watkinsmedia.org>
2025-02-19 15:16:28 +0000

    Fix bug in Backgammon Lifts not ending turn when game ends

--- src/main/scala/backgammon/Game.scala
+++ src/main/scala/backgammon/Game.scala
@@ -75,7 +75,7 @@ case class Game(
   def applyLift(lift: Lift): Game = {
     val newSituation = lift.situationAfter
     // if the lift triggers the end of the game we want to end the turn
-    val switchPlayer = situation.player != newSituation.player || situation.end
+    val switchPlayer = situation.player != newSituation.player || newSituation.end
 
     copy(
       situation = newSituation,
