4784d18f1ff16bb5c54dedd3b85296e19a1fbc44
th555 <thijs.meijerink@gmail.com>
2025-04-03 11:56:02 +0200

    more tests for men without captures

--- src/test/scala/dameo/DameoActorTest.scala
+++ src/test/scala/dameo/DameoActorTest.scala
@@ -1,11 +1,12 @@
 package strategygames.dameo
 
+import strategygames.dameo.format.FEN
+
 import org.specs2.matcher.ValidatedMatchers
 
 class DameoActorTest extends DameoTest with ValidatedMatchers {
   "man without captures" should {
-    val fen = variant.Dameo.initialFen
-    val board = Board(fen.pieces, variant.Dameo)
+    val board = Board(variant.Dameo.initialFen.pieces, variant.Dameo)
     val actors1 = Situation(board, P1).actors
     val actors2 = Situation(board, P2).actors
 
@@ -45,22 +46,62 @@ class DameoActorTest extends DameoTest with ValidatedMatchers {
       act2.captures.length must_== 0
     }
 
+    "have linear moves blocked by board edges" in {
+      val board = Board(FEN("W:Wa1,a2,b1,b2,c1,c2,g1,g2,h1,h2:Ba7,a8,b7,b8,g7,g8,h7,h8:H0:F1").pieces, variant.Dameo)
+      val actors1 = Situation(board, P1).actors
+      val actors2 = Situation(board, P2).actors
+
+      val act1l = actors1.find(_.pos == Pos.B1).get
+      val act1r = actors1.find(_.pos == Pos.G1).get
+      val act2l = actors2.find(_.pos == Pos.B8).get
+      val act2r = actors2.find(_.pos == Pos.G8).get
+      act1l.noncaptures.length must_== 2
+      act1r.noncaptures.length must_== 2
+      act2l.noncaptures.length must_== 2
+      act2r.noncaptures.length must_== 2
+
+      act1l.captures.length must_== 0
+      act1r.captures.length must_== 0
+      act2l.captures.length must_== 0
+      act2r.captures.length must_== 0
+    }
+
+    "have moves blocked by opponent pieces" in {
+      val board = Board(FEN("W:Wc2,c3,d2,d3,e2,e3:Bd4,d5,e4,e5,f4,f5:H0:F1").pieces, variant.Dameo)
+      val actors1 = Situation(board, P1).actors
+      val actors2 = Situation(board, P2).actors
+
+      actors1.find(_.pos == Pos.C3).get.noncaptures.length must_== 2
+      actors1.find(_.pos == Pos.D3).get.noncaptures.length must_== 1
+      actors1.find(_.pos == Pos.E3).get.noncaptures.length must_== 0
+
+      actors2.find(_.pos == Pos.F4).get.noncaptures.length must_== 2
+      actors2.find(_.pos == Pos.E4).get.noncaptures.length must_== 1
+      actors2.find(_.pos == Pos.D4).get.noncaptures.length must_== 0
+    }
+
+    "have no linear moves when blocked by own king" in {
+      val board = Board(FEN("W:Wa2.k,b1,b2.k,b3,c2,d3.k:Bc6.k,d7,e6,e7.k,e8,f7.k:H0:F1").pieces, variant.Dameo)
+      val actors1 = Situation(board, P1).actors
+      val actors2 = Situation(board, P2).actors
+
+      actors1.find(_.pos == Pos.B1).get.noncaptures.length must_== 0
+      actors2.find(_.pos == Pos.E8).get.noncaptures.length must_== 0
+    }
   }
 }
 /*
-    "have linear moves blocked by board edge" in {
+
+  "king without captures" should {
+    "move in all directions" in {
 
     }
 
-    "have moves blocked by other pieces" in {
-    }
-
-
-    "have linear moves blocked by other pieces" in {
+    "have n moves" in {
 
     }
 
-    "have no linear move when blocked by a king" in {
+    "not partake in linear movement" in {
 
     }
   }
@@ -75,20 +116,6 @@ class DameoActorTest extends DameoTest with ValidatedMatchers {
     }
 
     "have 2 equally long capture sequences" in {
-
-    }
-  }
-
-  "king without captures" should {
-    "move in all directions" in {
-
-    }
-
-    "have n moves" in {
-
-    }
-
-    "not partake in linear movement" in {
 
     }
   }
