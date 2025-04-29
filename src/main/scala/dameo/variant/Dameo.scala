308dd08c64eb6b2f036c7e6258e16bc0c229ef46
th555 <thijs.meijerink@gmail.com>
2025-04-29 16:12:27 +0200

    add initial FEN

--- src/main/scala/dameo/variant/Dameo.scala
+++ src/main/scala/dameo/variant/Dameo.scala
@@ -2,6 +2,7 @@ package strategygames.dameo
 package variant
 
 import strategygames.dameo._
+import strategygames.dameo.format.FEN
 import strategygames.GameFamily
 
 case object Dameo
@@ -19,4 +20,8 @@ case object Dameo
   def perfId: Int    = 800
 
   override def baseVariant: Boolean = true
+
+  override def initialFen: FEN = FEN(
+    "W:Wa1,b1,b2,c1,c2,c3,d1,d2,d3,e1,e2,e3,f1,f2,f3,g1,g2,h1:Ba8,b7,b8,c6,c7,c8,d6,d7,d8,e6,e7,e8,f6,f7,f8,g7,g8,h8:H0:F1"
+  )
 }
