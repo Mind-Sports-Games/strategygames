b2ff900543cd84750d4dcb42f93636be00b4e2b8
Matt Tucker <matt.watkinsmedia.org>
2025-11-27 11:14:15 +0000

    Make fairysf Situation.playable consider inverse check

--- src/main/scala/fairysf/Situation.scala
+++ src/main/scala/fairysf/Situation.scala
@@ -55,7 +55,7 @@ case class Situation(board: Board, player: Player) {
 
   def winner: Option[Player] = board.variant.winner(this)
 
-  def playable(strict: Boolean): Boolean = board.valid(strict) && !end
+  def playable(strict: Boolean): Boolean = board.valid(strict) && !end && !(!this.check)
 
   lazy val status: Option[Status] =
     if (checkMate) Status.Mate.some
